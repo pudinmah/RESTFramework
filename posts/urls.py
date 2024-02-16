5bf1d12d8f4d04eb8713301ecf3954788f2479d2
Mahpudin <pudinmah69@gmail.com>
2024-02-16 18:27:44 +0700

    Using serializers for CRUD

--- posts/urls.py
+++ posts/urls.py
@@ -3,6 +3,13 @@ from django.urls import path
 
 urlpatterns = [
     path("homepage/",views.homepage,name="posts_home"),
+
     path("",views.list_post,name="list_posts"),
-    path("<int:post_index>",views.post_detail,name="post_detail"),
+
+    path("<int:post_id>",views.post_detail,name="post_detail"),
+
+    path("update/<int:post_id>/",views.update_post,name='update_post'),
+
+    path("delete/<int:post_id>/", views.delete_post,name="delete_post")
+
 ]
