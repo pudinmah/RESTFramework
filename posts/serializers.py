9bdc299d4a43dfca26285d84439845a7bc4a2fc5
Mahpudin <pudinmah69@gmail.com>
2024-02-16 08:16:08 +0700

    Model serializers

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,8 +1,8 @@
 from rest_framework import serializers
+from .models import Post
 
 
-class PostSerializer(serializers.Serializer):
-    id= serializers.IntegerField(read_only=True)
-    title= serializers.CharField(max_length=50)
-    content= serializers.CharField()
-    created= serializers.DateTimeField(read_only=True)
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ["id","title","content","created"]
