4a535dedee822a84df1cf864c28c160f9bc3b015
Mahpudin <pudinmah69@gmail.com>
2024-02-17 03:34:18 +0700

    Creating a custom User Authentication model

--- simpleblog/settings.py
+++ simpleblog/settings.py
@@ -38,9 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'posts',
+    'accounts',
 
     'rest_framework',
 ]
+
+AUTH_USER_MODEL = "accounts.User"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
