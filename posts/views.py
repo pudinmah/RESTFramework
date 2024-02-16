9437792aff17abbfd0c0815607f0f3e71d09ee15
Mahpudin <pudinmah69@gmail.com>
2024-02-16 23:37:27 +0700

    viewsets.ModelViewSet

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from rest_framework import viewsets, status
-from rest_framework.request import Request
-from rest_framework.response import Response
+from rest_framework import viewsets
 from posts.serializers import PostSerializer
 from .models import Post
 
@@ -18,15 +16,6 @@ Viewset
 -.
 """
 
-class PostViewset(viewsets.ViewSet):
-    def list(self, request:Request):
-        queryset=Post.objects.all()
-        serializer= PostSerializer(instance=queryset,many=True)
-        return Response(data=serializer.data,status=status.HTTP_200_OK)
-    
-    def retrieve(self, request:Request, pk=None):
-        post = get_object_or_404(Post, pk=pk)
-
-        serializer= PostSerializer(instance=post)
-
-        return Response(data=serializer.data,status=status.HTTP_200_OK)
+class PostViewset(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
