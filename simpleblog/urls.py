782a4b7f46b322710b893887c9ed32e4a3e2a1c3
Mahpudin <pudinmah69@gmail.com>
2024-02-17 06:52:57 +0700

    User Account Creation (Signing Up)

--- simpleblog/urls.py
+++ simpleblog/urls.py
@@ -5,4 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('posts/',include("posts.urls")),
+    path("auth/",include("accounts.urls")),
 ]
