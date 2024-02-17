80a88e330c1b6f070e769fee92d0ba2cd185afac
Mahpudin <pudinmah69@gmail.com>
2024-02-18 02:13:08 +0700

    JWT Authentication

--- accounts/views.py
+++ accounts/views.py
@@ -7,6 +7,7 @@ from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.views import APIView
+from .tokens import create_jwt_pair_for_user
 
 
 
@@ -48,9 +49,13 @@ class LoginView(APIView):
         user= authenticate(email=email, password=password)
 
         if user is not None:
+
+            tokens=create_jwt_pair_for_user(user)
+
+
             response={
                 "message":"Login Successfully",
-                "token":user.auth_token.key
+                "tokens":tokens
             }
             return Response(data=response,status=status.HTTP_200_OK)
         
